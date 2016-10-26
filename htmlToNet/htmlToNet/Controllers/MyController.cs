a4a65fa8dbd9c53fb9f42359c0368952171548bd
Ting <Ting@DESKTOP-MJSRLVH>
2016-10-26 15:51:14 -0400

    creating a for loop

---
+++ htmlToNet/htmlToNet/Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace htmlToNet.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index(string name = "Ting", int id = 1)
+        {
+            ViewBag.Name = name;
+            ViewBag.ID = id;
+
+            return View();
+        }
+    }
+}
